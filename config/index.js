faeea0bdb37574ece6412586cdf75b8347dbbb1d
guma <vuster8@gmail.com>
2016-12-05 00:41:31 +0900

    set ip

--- config/index.js
+++ config/index.js
@@ -6,14 +6,16 @@
 var path = require('path');
 var rootPath = path.normalize(__dirname + '/..');
 var port = '8080';
+var ip = '0.0.0.0';
 if (process.env.NODE_ENV !== 'production') {
-    port = '1111';
+  port = '1111';
+  ip = '127.0.0.1';
 }
 
 module.exports = {
-    root: rootPath,
-    webServer: {
-        ip: '127.0.0.1',
-        port: port
-    }
+  root: rootPath,
+  webServer: {
+    ip: ip,
+    port: port
+  }
 };
